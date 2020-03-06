1e160e3425a6be8bf4065d0b29e25d3fd4cd5c3c
Erica Murdock-Waters <ericahpa@gmail.com>
2020-03-06 17:15:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,18 +22,18 @@ class Artist
     @@all << self
   end
 
-  def self.create(name)
-    artist = self.new(name)
-    artist.save
-    artist
-  end
+  # def self.create(name)
+  #   artist = self.new(name)
+  #   artist.save
+  #   artist
+  # end
 
   def self.find(name)
     self.all.detect {|artist| artist.name == name}
   end
 
   def self.find_or_create_by_name(name)
-    self.find(name) || self.create(name)
+    self.find(name) || self.new(name)
   end
 
   def print_songs
