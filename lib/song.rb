fcd9ad199f834234cab16f1421ebd18fb1e38ce3
Erica Murdock-Waters <ericahpa@gmail.com>
2020-03-06 16:31:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   end
 
   def artist_name=(artist)
-    self.artist = Artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(artist)
     artist.add_song(self)
   end
 
